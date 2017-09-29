b047451f52cc0b48f0152f5bc76d6257fa0fe8e3
leibo123 <leon_lei@brown.edu>
2017-09-28 21:48:38 -0400

    Update PersonPair.cs

--- PersonPair.cs
+++ PersonPair.cs
@@ -4,43 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LenaUbi
-{
-    class PersonPair
-    {
+namespace LenaUbi {
+    class PersonPair {
         private double interactions = 0;
-        public double Interactions
-        {
-            get
-            {
+        public double Interactions {
+            get {
                 return this.interactions;
             }
-            set
-            {
+            set {
                 this.interactions = value;
             }
         }
         private Person person1 = new Person();
-        public Person Person1
-        {
-            get
-            {
+        public Person Person1 {
+            get {
                 return this.person1;
             }
-            set
-            {
+            set {
                 this.person1 = value;
             }
         }
         private Person person2 = new Person();
-        public Person Person2
-        {
-            get
-            {
+        public Person Person2 {
+            get {
                 return this.person2;
             }
-            set
-            {
+            set {
                 this.person2 = value;
             }
         }
